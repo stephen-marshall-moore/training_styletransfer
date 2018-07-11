86f33e3a82001965c58f41d3172be1f11fee86eb
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-07-11 15:59:03 -0400

    change coco dateset dir

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 python style.py --style images/matildeperez.jpg \
   --checkpoint-dir checkpoints/ \
+  --train-path /datasets/coco \
   --model-dir models/ \
   --test images/violetaparra.jpg \
   --test-dir tests/ \
